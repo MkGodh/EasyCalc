7732b9156e3b61ef21ffd3c45150d0ae3b0767b0
MkGodh <fenryrvla@yandex.ru>
2024-02-22 18:02:51 +0300

    Добавьте файлы проекта.

---
+++ PracticePlace2/PracticePlace2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int numbers;
+    int count = 0;
+    int countTwo = 0;
+    cout << "Сколько чисел вы хотите сложить? "; cin >> numbers;
+    while (count != numbers) {
+        int countOne;
+        cout << "Введите число: "; cin >> countOne;
+        countTwo += countOne;
+        
+        count++;
+        
+    }
+    cout << countTwo;
+}
